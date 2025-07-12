b7215edfdc14330d51929b873f6d57b8b52033ff
ablckone16 <herouknow830116@gmail.com>
2025-07-12 22:11:49 +0800

    bug修復 新增JS互動 漢堡選單

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
 $(function(){
+
+    /* -------------------- 漢堡選單控制 -------------------- */
     $('#burger').click(function(){
         $(this).toggleClass('active');
-    })
+        $('.rwd-menu').toggleClass('active');
+    });
+
+    $('#burger-in').click(function(){
+        $('#burger').toggleClass('active');
+        $('.rwd-menu').toggleClass('active');
+    });
+
+
+    /* -------------------- 首頁 intro 區卡片 -------------------- */
+
+    $('.drop-toggle-btn').click(function () {
+        const card = $(this).closest('.card');
+        const dropTextBox = card.find('.card-text-box');
+        
+        dropTextBox.toggleClass('active');
+    });
+
+    /* -------------------- 蟲圖鑑卡片 -------------------- */
+
+    $('.beetle-card').click(function () {
+        if ($(this).hasClass('active')) {
+          $(this).removeClass('active');
+        } else {
+          $('.beetle-card').removeClass('active');
+          $(this).addClass('active');
+        }
+    });
+
+    /* -------------------- FAQs-list控制 -------------------- */
+
+    $('.faq-toggle-btn').click(function () {
+        const faqItem = $(this).closest('.faq-item');
+        
+        faqItem.toggleClass('active');
+    });
+
 });
